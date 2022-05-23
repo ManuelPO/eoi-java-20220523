34e55fe9cbc6fdd87961793ea271804a9572e9f6
Manuel <manuel.prietoor@gmail.com>
2022-05-23 17:03:21 +0200

    Counter.java created

---
+++ Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+    public static void main(String[] args) {
+        contarMil();
+    }
+
+    public static void contarMil(){
+        for(int i = 1; i <=1000; i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
